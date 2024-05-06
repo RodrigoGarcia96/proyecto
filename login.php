9baf746297b391a49d9f63ed8662572c03e644e8
Rodrigo Garcia <garciarod1408@gmail.com>
2024-05-06 13:17:09 -0600

    Creacion de la conexión a la bd

---
+++ login.php
@@ -0,0 +1,21 @@
+<?php
+include 'config.php';
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+
+    $sql = "SELECT * FROM usuarios WHERE username='$username' AND password='$password'";
+    $result = $conn->query($sql);
+
+    if ($result && $result->num_rows == 1) {
+        // Inicio de sesión exitoso
+        // Redirigir al usuario a la página deseada
+        header("Location: puntoVenta.php");
+        exit;
+    } else {
+        // Inicio de sesión fallido
+        echo "Inicio de sesión fallido";
+    }
+}
+?>
